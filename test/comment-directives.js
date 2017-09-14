8742a3aa01c50ae43d19a52635297ce7774fc78c
Ilya Drabenia <ilya.drabenia@altoros.com>
2017-09-14 18:21:25 +0300

    13-implement-possibilities-to-disable-validation-using-comments

---
+++ test/comment-directives.js
@@ -0,0 +1,67 @@
+const assert = require('assert');
+const linter = require('./../lib/index');
+
+
+describe('Linter', function() {
+    describe('Comment Directives', function () {
+
+        it('should disable fixed compiler error', function () {
+            const report = linter.processStr('pragma solidity ^0.4.4; // solhint-disable-line');
+
+            assert.equal(report.errorCount, 0);
+        });
+
+        it('should disable fixed compiler error using multiline comment', function () {
+            const report = linter.processStr('pragma solidity ^0.4.4; /* solhint-disable-line */');
+
+            assert.equal(report.errorCount, 0);
+        });
+
+        it('should disable only one compiler error', function () {
+            const report = linter.processStr(`
+                // solhint-disable-next-line
+                pragma solidity ^0.4.4; 
+                pragma solidity 0.3.4;
+            `);
+
+            assert.equal(report.errorCount, 1);
+        });
+
+        it('should disable only compiler version error', function () {
+            const report = linter.processStr(`
+                // solhint-disable compiler-gt-0_4
+                pragma solidity ^0.4.4; 
+                pragma solidity 0.3.4; // disabled error: Compiler version must be greater that 0.4
+            `);
+
+            assert.equal(report.errorCount, 1);
+            assert.ok(report.reports[0].message.includes('Compiler version must be fixed'));
+        });
+
+
+        it('should disable only one compiler version error', function () {
+            const report = linter.processStr(`
+                /* solhint-disable compiler-gt-0_4 */
+                pragma solidity 0.3.4;
+                /* solhint-enable compiler-gt-0_4 */
+                pragma solidity 0.3.4; 
+            `);
+
+            assert.equal(report.errorCount, 1);
+            assert.ok(report.reports[0].message.includes('0.4'));
+        });
+
+        it('should not disable fixed compiler error', function () {
+            const report = linter.processStr(`
+                /* solhint-disable compiler-gt-0_4 */
+                pragma solidity ^0.4.4;
+                /* solhint-enable compiler-gt-0_4 */
+                pragma solidity ^0.4.4; 
+            `);
+
+            assert.equal(report.errorCount, 2);
+            assert.ok(report.reports[0].message.includes('fixed'));
+        });
+
+    });
+});
