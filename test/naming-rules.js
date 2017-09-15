c12a0bf82d7b3cf18581c58640dd2a0af9d99e1c
Ilya Drabenia <ilya.drabenia@altoros.com>
2017-09-15 19:17:42 +0300

    33-local-and-state-variables-checker

--- test/naming-rules.js
+++ test/naming-rules.js
@@ -49,6 +49,23 @@ describe('Linter', function() {
             assert.ok(report.messages.map(i => i.message).some(i => i.includes('name')));
         });
 
+        it('should raise incorrect var name error for state declaration', function () {
+            const code = contractWith('uint32 private D = 10;');
+
+            const report = linter.processStr(code);
+
+            assert.equal(report.errorCount, 1);
+            assert.ok(report.messages[0].message.includes('Variable name'));
+        });
+
+        it('should not raise var name error for constants', function () {
+            const code = contractWith('uint32 private constant D = 10;');
+
+            const report = linter.processStr(code);
+
+            assert.equal(report.errorCount, 0);
+        });
+
     });
 
     function contractWith(code) {
