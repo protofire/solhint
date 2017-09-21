22fab8fb3e0bcaa3a264a823a806f5f2cd09a538
Ilya Drabenia <ilya.drabenia@altoros.com>
2017-09-21 15:17:29 +0300

    22-implement-whitespaces-in-expression-checker

--- test/expression-rules.js
+++ test/expression-rules.js
@@ -7,12 +7,21 @@ describe('Linter', function() {
     describe('Expression Align Rules', function () {
 
         it('should raise error when "new" has more than one space', function () {
-            const code = funcWith('new  TrustedContract');
+            const code = funcWith('new  TrustedContract;');
 
             const report = linter.processStr(code, config());
 
             assert.equal(report.errorCount, 1);
             assert.ok(report.messages[0].message.includes('Required space'));
+        });
+
+        it('should raise error when array access brackets has spaces', function () {
+            const code = funcWith('myArray[ 5 ];');
+
+            const report = linter.processStr(code, config());
+
+            assert.equal(report.errorCount, 1);
+            assert.ok(report.messages[0].message.includes('Expression indentation is incorrect'));
         });
 
     });
