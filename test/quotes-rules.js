8aa83428cffe495678843254f642e5b3d0418014
Ilya Drabenia <ilya.drabenia@altoros.com>
2017-09-25 17:28:19 +0300

    9-pack-as-npm-package

--- test/quotes-rules.js
+++ test/quotes-rules.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
 const linter = require('./../lib/index');
-const contractWith = require('./contract-builder').contractWith;
+const { contractWith, funcWith } = require('./contract-builder');
 
 
 describe('Linter', function() {
@@ -46,6 +46,15 @@ describe('Linter', function() {
             const code = 'import {a as b, c as d} from \'lib.sol\';';
 
             const report = linter.processStr(code);
+
+            assert.equal(report.errorCount, 1);
+            assert.ok(report.messages[0].message.includes('double quotes'));
+        });
+
+        it('should raise quotes error in assembly clause', function () {
+            const code = funcWith('assembly { \'abc\' }');
+
+            const report = linter.processStr(code, config());
 
             assert.equal(report.errorCount, 1);
             assert.ok(report.messages[0].message.includes('double quotes'));
