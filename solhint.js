bf1790c165c465ccabc4aa33c683069fd34306d6
Ilya Drabenia <ilya.drabenia@altoros.com>
2017-12-01 12:40:08 +0300

    Release v1.1.9

--- solhint.js
+++ solhint.js
@@ -9,7 +9,7 @@ const process = require('process');
 
 function init () {
     program
-        .version('1.1.8');
+        .version('1.1.9');
 
     program
         .usage('[options] <file> [...other_files]')
