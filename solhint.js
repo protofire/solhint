21d8b4e139d1016cdeb583c499a5088240934f7b
Ilya Drabenia <ilya.drabenia@altoros.com>
2018-01-16 14:22:16 +0300

    Prepare for release 1.1.10

--- solhint.js
+++ solhint.js
@@ -9,7 +9,7 @@ const process = require('process');
 
 function init() {
     program
-        .version('1.1.9');
+        .version('1.1.10');
 
     program
         .usage('[options] <file> [...other_files]')
