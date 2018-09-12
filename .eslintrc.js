426c72a5670a9584ebc5073182251c8b6ae01344
Franco Victorio <victorio.franco@gmail.com>
2018-09-12 11:32:57 -0300

    Add prettier and airbnb-base plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,16 @@
-
 module.exports = {
-    "parserOptions": {
-        "ecmaVersion": 8
-    },
-    "env": {
-        "browser": false,
-        "node": true,
-        "commonjs": true,
-        "es6": true,
-        "mocha": true
-    },
-    "extends": "eslint:recommended",
-    "rules": {
-        "no-console": 0,
-        "indent": [
-            "error",
-            4
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ],
-        "space-before-function-paren": [
-            "error",
-            {"anonymous": "always", "named": "never"}
-        ],
-        "max-len": [
-            "error",
-            120
-        ]
-    }
-};
+  parserOptions: {
+    ecmaVersion: 8
+  },
+  env: {
+    browser: false,
+    node: true,
+    commonjs: true,
+    es6: true,
+    mocha: true
+  },
+  extends: ['airbnb-base', 'plugin:prettier/recommended'],
+  rules: {
+    'no-console': 'off'
+  }
+}
