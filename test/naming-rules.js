8098ecbc30af0cd46a0dd645cf3bbb42d19e8f37
Nicolás Venturo <nicolas.venturo@gmail.com>
2018-12-21 13:33:42 -0300

    The snake case check now allows for up to two leading underscores.

--- test/naming-rules.js
+++ test/naming-rules.js
@@ -67,12 +67,29 @@ describe('Linter', () => {
       assert.equal(report.errorCount, 0)
     })
 
-    it('should not raise var name error for constants in snake case with leading underscore', () => {
+    it('should not raise var name error for constants in snake case with single leading underscore', () => {
       const code = contractWith('uint32 private constant _THE_CONSTANT = 10;')
 
       const report = linter.processStr(code, noIndent())
 
       assert.equal(report.errorCount, 0)
+    })
+
+    it('should not raise var name error for constants in snake case with double leading underscore', () => {
+      const code = contractWith('uint32 private constant __THE_CONSTANT = 10;')
+
+      const report = linter.processStr(code, noIndent())
+
+      assert.equal(report.errorCount, 0)
+    })
+
+    it('should not var name error for constants in snake case with more than two leading underscores', () => {
+      const code = contractWith('uint32 private constant ___THE_CONSTANT = 10;')
+
+      const report = linter.processStr(code, noIndent())
+
+      assert.equal(report.errorCount, 1)
+      assert.ok(report.messages[0].message.includes('SNAKE_CASE'))
     })
 
     it('should raise var name error for event arguments illegal styling', () => {
