2a8ba9ed3581a732b772e5178ab4936cd60f33eb
fernandomg <fernando.greco@gmail.com>
2019-11-29 16:28:01 -0300

    Update rule: `no-unused-vars`

--- test/rules/best-practises/no-unused-vars.js
+++ test/rules/best-practises/no-unused-vars.js
@@ -28,7 +28,13 @@ describe('Linter - no-unused-vars', () => {
     contractWith('function a(uint d) public returns (uint c) { }'),
     contractWith('function a(uint a, uint c) public returns (uint c);'),
     contractWith(
-      'function a(address a) internal { assembly { t := eq(a, and(mask, calldataload(4))) } }'
+      multiLine(
+        'function a(address a) internal {',
+        '  assembly {',
+        '    let t := eq(a, and(mask, calldataload(4)))',
+        '  }',
+        '}'
+      )
     ),
     contractWith(
       multiLine(
@@ -65,60 +71,6 @@ describe('Linter - no-unused-vars', () => {
       assertNoWarnings(report)
     })
   )
-
-  it('should raise incorrect var name error', () => {
-    const code = funcWith('var (a, B);')
-
-    const report = linter.processStr(code, {
-      rules: { 'no-unused-vars': 'error', 'var-name-mixedcase': 'error' }
-    })
-
-    assert.ok(report.errorCount > 0)
-    assert.ok(report.messages.map(i => i.message).some(i => i.includes('name')))
-  })
-
-  it('should raise incorrect var name error for typed declaration', () => {
-    const code = funcWith('uint B = 1;')
-
-    const report = linter.processStr(code, {
-      rules: { 'no-unused-vars': 'error', 'var-name-mixedcase': 'error' }
-    })
-
-    assert.ok(report.errorCount > 0)
-    assert.ok(report.messages.map(i => i.message).some(i => i.includes('name')))
-  })
-
-  it('should raise incorrect var name error for state declaration', () => {
-    const code = contractWith('uint32 private D = 10;')
-
-    const report = linter.processStr(code, {
-      rules: { 'no-unused-vars': 'error', 'var-name-mixedcase': 'error' }
-    })
-
-    assert.equal(report.errorCount, 1)
-    assert.ok(report.messages[0].message.includes('Variable name'))
-  })
-
-  it('should not raise var name error for constants', () => {
-    const code = contractWith('uint32 private constant D = 10;')
-
-    const report = linter.processStr(code, {
-      rules: { 'no-unused-vars': 'error', 'var-name-mixedcase': 'error' }
-    })
-
-    assert.equal(report.errorCount, 0)
-  })
-
-  it('should raise forbidden name error', () => {
-    const code = funcWith('uint l = 0;')
-
-    const report = linter.processStr(code, {
-      rules: { 'no-unused-vars': 'error', 'use-forbidden-name': 'error' }
-    })
-
-    assert.equal(report.errorCount, 2)
-    assert.ok(report.messages[0].message.includes('Avoid to use'))
-  })
 
   function label(data) {
     const items = data.split('\n')
