884683135adcb6c57608a032cfe5eec8fa665bf4
Franco Victorio <victorio.franco@gmail.com>
2020-10-15 11:33:14 -0300

    Add test for stdin

--- e2e/test.js
+++ e2e/test.js
@@ -3,7 +3,9 @@
 /* eslint-disable no-unused-expressions */
 
 const {expect} = require('chai')
+const cp = require('child_process')
 const fs = require('fs-extra')
+const getStream = require('get-stream')
 const os = require('os')
 const path = require('path')
 const shell = require('shelljs')
@@ -72,6 +74,41 @@ describe('e2e', function() {
       expect(code).to.equal(0)
 
       expect(stdout.trim()).to.equal('Configuration file already exists')
+    })
+  })
+
+  describe('no-empty-blocks', function() {
+    useFixture('03-no-empty-blocks')
+
+    it('should exit with 1', function() {
+      const {code, stdout} = shell.exec('solhint Foo.sol')
+
+      expect(code).to.equal(1)
+
+      expect(stdout.trim()).to.contain('Code contains empty blocks')
+    })
+
+    it('should work with stdin', async function() {
+      const child = cp.exec('solhint stdin')
+
+      const stdoutPromise = getStream(child.stdout)
+
+      const codePromise = new Promise(resolve => {
+        child.on('close', code => {
+          resolve(code)
+        })
+      })
+
+      child.stdin.write('contract Foo {}')
+      child.stdin.end()
+
+      const code = await codePromise
+
+      expect(code).to.equal(1)
+
+      const stdout = await stdoutPromise
+
+      expect(stdout.trim()).to.equal('Code contains empty blocks')
     })
   })
 })
