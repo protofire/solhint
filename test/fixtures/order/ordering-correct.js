afb215b88d5ef9ed3c3afa4b52d4e1aad4fee96e
fernandomg <fernando.greco@gmail.com>
2020-12-18 21:00:34 -0300

    add tests for `ordering` rule
    - also fixed the `const` -> `constant` modifier

--- test/fixtures/order/ordering-correct.js
+++ test/fixtures/order/ordering-correct.js
@@ -1,8 +1,8 @@
 module.exports = [
   {
-    description: 'All units are in order',
+    description: 'All units are in order - ^0.4.0',
     code: `
-pragma solidity ^0.6.0;
+pragma solidity ^0.4.0;
 
 import "./some/library.sol";
 import "./some/other-library.sol";
@@ -47,11 +47,89 @@ contract MyContract {
   fallback () external {}
 
   function myExternalFunction() external {}
-  function myExternalConstFunction() external const {}
+  function myExternalConstantFunction() external constant {}
+
   function myPublicFunction() public {}
-  function myPublicConstFunction() public const {}
+  function myPublicConstantFunction() public constant {}
+
   function myInternalFunction() internal {}
   function myPrivateFunction() private {}
+}
+`
+  },
+  {
+    description: 'All units are in order - ^0.5.0',
+    code: `
+pragma solidity ^0.5.0;
+
+import "./some/library.sol";
+import "./some/other-library.sol";
+
+enum MyEnum {
+  Foo,
+  Bar
+}
+
+struct MyStruct {
+  uint x;
+  uint y;
+}
+
+interface IBox {
+  function getValue() public;
+  function setValue(uint) public;
+}
+
+library MyLibrary {
+  function add(uint a, uint b, uint c) public returns (uint) {
+    return a + b + c;
+  }
+}
+
+contract MyContract {
+  using MyLibrary for uint;
+
+  struct InnerStruct {
+    bool flag;
+  }
+
+  enum InnerEnum {
+    A, B, C
+  }
+
+  address payable owner;
+  uint public x;
+  uint public y;
+
+  event MyEvent(address a);
+
+  modifier onlyOwner {
+    require(
+      msg.sender == owner,
+      "Only owner can call this function."
+    );
+    _;
+  }
+
+  constructor () public {}
+
+  fallback () external {}
+
+  function myExternalFunction() external {}
+  function myExternalViewFunction() external view {}
+  function myExternalPureFunction() external pure {}
+
+  function myPublicFunction() public {}
+  function myPublicViewFunction() public view {}
+  function myPublicPureFunction() public pure {}
+
+  function myInternalFunction() internal {}
+  function myInternalViewFunction() internal view {}
+  function myInternalPureFunction() internal pure {}
+
+  function myPrivateFunction() private {}
+  function myPrivateViewFunction() private view {}
+  function myPrivatePureFunction() private pure {}
 }
 `
   }
