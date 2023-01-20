bed4264f2096c4de42b806075598224f1867d477
dbale-altoros <diego.bale@altoros.com>
2023-01-20 10:57:30 -0300

    fix foundry imports

--- test/rules/best-practises/no-console.js
+++ test/rules/best-practises/no-console.js
@@ -3,6 +3,9 @@ const linter = require('./../../../lib/index')
 const { assertErrorMessage } = require('./../../common/asserts')
 const { funcWith } = require('./../../common/contract-builder')
 
+const FUNCTION_CALL_ERROR = 'No console.logX or console2.log statements'
+const IMPORT_ERROR = 'No import "hardhat/console.sol" or "forge-std/console.sol" statements'
+
 describe('Linter - no-console', () => {
   it('should raise console.log() is not allowed', () => {
     const code = funcWith(`
@@ -14,7 +17,7 @@ describe('Linter - no-console', () => {
     })
 
     assert.equal(report.errorCount, 1)
-    assertErrorMessage(report, 'No console.logX statements')
+    assertErrorMessage(report, FUNCTION_CALL_ERROR)
   })
 
   it('should raise console.logString() is not allowed', () => {
@@ -27,7 +30,7 @@ describe('Linter - no-console', () => {
     })
 
     assert.equal(report.errorCount, 1)
-    assertErrorMessage(report, 'No console.logX statements')
+    assertErrorMessage(report, FUNCTION_CALL_ERROR)
   })
 
   it('should raise console.logBytes12() is not allowed', () => {
@@ -40,10 +43,23 @@ describe('Linter - no-console', () => {
     })
 
     assert.equal(report.errorCount, 1)
-    assertErrorMessage(report, 'No console.logX statements')
+    assertErrorMessage(report, FUNCTION_CALL_ERROR)
   })
 
-  it('should raise hardhat import console.sol is not allowed', () => {
+  it('should raise console2.log is not allowed', () => {
+    const code = funcWith(`
+      console2.log('test');
+    `)
+
+    const report = linter.processStr(code, {
+      rules: { 'no-console': ['error'] }
+    })
+
+    assert.equal(report.errorCount, 1)
+    assertErrorMessage(report, FUNCTION_CALL_ERROR)
+  })
+
+  it('should raise hardhat import/console.sol is not allowed', () => {
     const code = `
     import "hardhat/console.sol";
     contract A {}
@@ -54,12 +70,12 @@ describe('Linter - no-console', () => {
     })
 
     assert.equal(report.errorCount, 1)
-    assertErrorMessage(report, 'No import "hardhat/console.sol" or "forge-std/xxx" statement')
+    assertErrorMessage(report, IMPORT_ERROR)
   })
 
-  it('should raise hardhat import forge-std/xxx is not allowed', () => {
+  it('should raise foundry forge-std/console.sol is not allowed', () => {
     const code = `
-    import "forge-std/zarasa.sol";
+    import "forge-std/console.sol";
     contract A {}
     `
 
@@ -68,6 +84,33 @@ describe('Linter - no-console', () => {
     })
 
     assert.equal(report.errorCount, 1)
-    assertErrorMessage(report, 'No import "hardhat/console.sol" or "forge-std/xxx" statement')
+    assertErrorMessage(report, IMPORT_ERROR)
+  })
+
+  it('should raise foundry import forge-std/console2.sol is not allowed', () => {
+    const code = `
+    import "forge-std/console2.sol";
+    contract A {}
+    `
+
+    const report = linter.processStr(code, {
+      rules: { 'no-console': ['error'] }
+    })
+
+    assert.equal(report.errorCount, 1)
+    assertErrorMessage(report, IMPORT_ERROR)
+  })
+
+  it('should NOT raise error to foundry import forge-std/otherThing.sol', () => {
+    const code = `
+    import "forge-std/xxxxx.sol";
+    contract A {}
+    `
+
+    const report = linter.processStr(code, {
+      rules: { 'no-console': ['error'] }
+    })
+
+    assert.equal(report.errorCount, 0)
   })
 })
