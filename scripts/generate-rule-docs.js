e5c1ec3ebfd20b41582604f674b950ee9f582521
dbale-altoros <diego.bale@altoros.com>
2023-07-07 15:14:41 -0300

    add deprecated mark on rules.md

--- scripts/generate-rule-docs.js
+++ scripts/generate-rule-docs.js
@@ -218,8 +218,8 @@ function getDefaultSeverity(rule) {
 
 function generateRuleIndex(rulesIndexed) {
     const contents = Object.keys(rulesIndexed).map(category => {
-        const rows = [["Rule Id", "Error", "Recommended"]];
-        rulesIndexed[category].map(rule => [`[${rule.ruleId}](./rules/${rule.meta.type}/${rule.ruleId}.md)`, rule.meta.docs.description, (rule.meta.recommended && !rule.meta.deprecated) ? '✔️' : '']).forEach(row => rows.push(row));
+        const rows = [["Rule Id", "Error", "Recommended", "Deprecated"]];
+        rulesIndexed[category].map(rule => [`[${rule.ruleId}](./rules/${rule.meta.type}/${rule.ruleId}.md)`, rule.meta.docs.description, (rule.meta.recommended && !rule.meta.deprecated) ? '$~~~~~~~~$✔️' : '', rule.meta.deprecated ? '$~~~~~~~$✔️' : '']).forEach(row => rows.push(row));
         return `## ${category}
 
 ${table(rows)}
