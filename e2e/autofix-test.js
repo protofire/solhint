ff5e3927e03095806067b053a08a8a4ab5302003
dbale-altoros <diego.bale@altoros.com>
2023-12-20 10:54:35 -0300

    test: e2e not workin 1

--- e2e/autofix-test.js
+++ e2e/autofix-test.js
@@ -25,7 +25,8 @@ function retrieveParams(subpath) {
 function compareTextFiles(file1Path, file2Path) {
   const file1Content = fs.readFileSync(file1Path, 'utf-8')
   const file2Content = fs.readFileSync(file2Path, 'utf-8')
-
+  console.log('file1Content :>> ', file1Content);
+  console.log('file2Content :>> ', file2Content);
   return file1Content === file2Content
 }
 
