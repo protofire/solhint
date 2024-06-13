2b2a0a45c56361e3f6e3a4f7ebdbb2eda444511d
dropbigfish <fillfish@foxmail.com>
2024-06-13 11:59:08 +0800

    chore: fix some comments
    Signed-off-by: dropbigfish <fillfish@foxmail.com>

--- test/rules/naming/func-named-parameters.js
+++ test/rules/naming/func-named-parameters.js
@@ -27,7 +27,7 @@ describe('Linter - func-named-parameters', () => {
       assertErrorCount(report, 1)
       assertErrorMessage(
         report,
-        `Named parameters missing. MIN unnamed argumenst is ${
+        `Named parameters missing. MIN unnamed arguments is ${
           minUnnamed < DEFAULT_MIN_UNNAMED_ARGUMENTS ? DEFAULT_MIN_UNNAMED_ARGUMENTS : minUnnamed
         }`
       )
@@ -90,7 +90,7 @@ describe('Linter - func-named-parameters', () => {
 
     assertErrorMessage(
       report,
-      `Named parameters missing. MIN unnamed argumenst is ${DEFAULT_MIN_UNNAMED_ARGUMENTS}`
+      `Named parameters missing. MIN unnamed arguments is ${DEFAULT_MIN_UNNAMED_ARGUMENTS}`
     )
   })
 
@@ -106,7 +106,7 @@ describe('Linter - func-named-parameters', () => {
 
     assertErrorMessage(
       report,
-      `Named parameters missing. MIN unnamed argumenst is ${DEFAULT_MIN_UNNAMED_ARGUMENTS}`
+      `Named parameters missing. MIN unnamed arguments is ${DEFAULT_MIN_UNNAMED_ARGUMENTS}`
     )
   })
 })
