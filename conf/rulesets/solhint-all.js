0ae84e34bececf3bc3d5dd0cd216e98fc91648a7
dbale-altoros <diego.bale@altoros.com>
2024-07-01 15:38:47 -0300

    add: import-order rule

--- conf/rulesets/solhint-all.js
+++ conf/rulesets/solhint-all.js
@@ -50,7 +50,7 @@ module.exports = Object.freeze({
         immutablesAsConstants: true,
       },
     ],
-    'import-order': ['warn', 4],
+    'import-order': 'warn',
     'modifier-name-mixedcase': 'warn',
     'named-parameters-mapping': 'warn',
     'private-vars-leading-underscore': [
