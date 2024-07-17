649dee8deea2e2ccf0af7a1d01edf022eb6073eb
dbale-altoros <diego.bale@altoros.com>
2024-07-17 09:14:50 -0300

    autofix e2e test11

--- e2e/08-autofix/imports-order/Foo1.sol
+++ e2e/08-autofix/imports-order/Foo1.sol
@@ -5,17 +5,19 @@ import './ThisIsAVeryLongFileOnPurposeToTestTheFirstPathShorterThanTheLastOneloo
 import { Unauthorized, add as func, Point } from './Foo.sol';
 import 'https://github.com/owner/repo/blob/branch/path/to/Contract.sol';
 import { Initializable } from './openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol';
-import './../../../../token/interfaces/FakeContract1.sol';
-import { FakeContract3 } from './../../../token/interfaces/FakeContract3.sol';
+import '../../../../token/interfaces/FakeContract1.sol';
+import { FakeContract3 } from '../../../token/interfaces/FakeContract3.sol';
 import './../../../../token/interfaces/AFakeContract1.sol';
 import './../token/interfaces/IXTokenWrapper.sol';
-import { IXTokenFactory, holaquetal } from './../../token/interfaces/IXTokenFactory.sol';
+import { IXTokenFactory, holaquetal } from '../../token/interfaces/IXTokenFactory.sol';
+import './../../bpath/otherfolder/otherfolder/aContract.sol';
 import '@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol';
-import { FakeContract2 } from './../../../token/interfaces/FakeContract2.sol';
+import { FakeContract2 } from '../../../token/interfaces/FakeContract2.sol';
+import './../../apath/zContract.sol';
 import 'http://github.com/owner/repo/blob/branch/path/to/Contract2.sol';
 import { Afool1 } from './Afool1.sol';
 import './Ownable.sol';
-import { IXTokenWrapper2 } from './../token/interfaces/IXTokenWrapper2.sol';
+import { IXTokenWrapper2 } from '../token/interfaces/IXTokenWrapper2.sol';
 import { ReentrancyGuardUpgradeable2 } from '@apenzeppelin/ReentrancyGuardUpgradeable2.sol';
 
 contract ImportsOrder {
--- e2e/08-autofix/imports-order/Foo1AfterFix.sol
+++ e2e/08-autofix/imports-order/Foo1AfterFix.sol
@@ -2,14 +2,16 @@
 pragma solidity ^0.8.0;
 
 import './../../../../token/interfaces/AFakeContract1.sol';
-import './../../../../token/interfaces/FakeContract1.sol';
-import { FakeContract2 } from './../../../token/interfaces/FakeContract2.sol';
-import { FakeContract3 } from './../../../token/interfaces/FakeContract3.sol';
-import { IXTokenFactory, holaquetal } from './../../token/interfaces/IXTokenFactory.sol';
+import '../../../../token/interfaces/FakeContract1.sol';
+import { FakeContract2 } from '../../../token/interfaces/FakeContract2.sol';
+import { FakeContract3 } from '../../../token/interfaces/FakeContract3.sol';
+import './../../apath/zContract.sol';
+import './../../bpath/otherfolder/otherfolder/aContract.sol';
+import { holaquetal, IXTokenFactory } from '../../token/interfaces/IXTokenFactory.sol';
 import './../token/interfaces/IXTokenWrapper.sol';
-import { IXTokenWrapper2 } from './../token/interfaces/IXTokenWrapper2.sol';
+import { IXTokenWrapper2 } from '../token/interfaces/IXTokenWrapper2.sol';
 import { Afool1 } from './Afool1.sol';
-import { Unauthorized, add as func, Point } from './Foo.sol';
+import { add as func, Point, Unauthorized } from './Foo.sol';
 import { Initializable } from './openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol';
 import './Ownable.sol';
 import './ThisIsAVeryLongFileOnPurposeToTestTheFirstPathShorterThanTheLastOnelooooooooooong.sol';
