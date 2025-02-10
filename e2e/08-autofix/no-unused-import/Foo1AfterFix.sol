216705a3524b6556791cb4210ee5de541e550c42
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2025-02-10 15:00:43 +0700

    fix: improve tests for no-unused-import

--- e2e/08-autofix/no-unused-import/Foo1AfterFix.sol
+++ e2e/08-autofix/no-unused-import/Foo1AfterFix.sol
@@ -2,7 +2,8 @@
 pragma solidity ^0.8.0;
 
 import { Bar, Zed as Nit } from '../Other.sol';
+import { Wyd } from '../Wyd.sol';
 
-contract Foo is Bar, Nit {
-    constructor() Bar() Nit(msg.sender) {}
+contract Foo is Bar, Nit, Wyd {
+    constructor() Bar() Nit(msg.sender) Wyd() {}
 }
--- e2e/08-autofix/no-unused-import/Foo1BeforeFix.sol
+++ e2e/08-autofix/no-unused-import/Foo1BeforeFix.sol
@@ -3,7 +3,8 @@ pragma solidity ^0.8.0;
 
 import { IERC20 } from '../token/interfaces/IERC20.sol';
 import { Bar, Civ, Zed as Nit } from '../Other.sol';
+import { Wyd, Mar } from '../Wyd.sol';
 
-contract Foo is Bar, Nit {
-    constructor() Bar() Nit(msg.sender) {}
+contract Foo is Bar, Nit, Wyd {
+    constructor() Bar() Nit(msg.sender) Wyd() {}
 }
