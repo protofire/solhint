b68a384e756c9ac244a7dda29b1009053394e409
dbale-altoros <diego.bale@altoros.com>
2025-03-04 16:56:30 -0300

    ci: skip failing test to debug 13

--- e2e/autofix-test.js
+++ e2e/autofix-test.js
@@ -1,12 +1,12 @@
 const chai = require('chai')
 const { expect } = chai
 const fs = require('fs-extra')
-// const os = require('os')
-// const path = require('path')
+const os = require('os')
+const path = require('path')
 const shell = require('shelljs')
 const spawnSync = require('spawn-sync')
 
-const { useFixture } = require('./_common/common')
+// const { useFixture } = require('./_common/common')
 
 const EXIT_CODES = { BAD_OPTIONS: 255, OK: 0, REPORTED_ERRORS: 1 }
 
@@ -580,3 +580,30 @@ describe('e2e', function () {
 //     shell.cd(this.testDirPath)
 //   })
 // }
+
+function useFixture(dir) {
+
+  console.log('\n\ndiR :>> ', dir);
+  
+  beforeEach(`switch to ${dir}`, function () {
+    const fixturePath = path.join(__dirname, dir).replace('/_common', '')
+    
+    console.log('\n\nfixturePath :>> ', fixturePath);
+    
+    const tmpDirContainer = os.tmpdir()
+    
+    console.log('tmpDirContainer :>> ', tmpDirContainer);
+    
+    this.testDirPath = path.join(tmpDirContainer, `solhint-tests-${dir}`)
+    
+    console.log('this.testDirPath :>> ', this.testDirPath);
+    console.log('\n\n');
+
+    fs.ensureDirSync(this.testDirPath)
+    fs.emptyDirSync(this.testDirPath)
+
+    fs.copySync(fixturePath, this.testDirPath)
+
+    shell.cd(this.testDirPath)
+  })
+}
