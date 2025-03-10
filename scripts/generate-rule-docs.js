2291f038e78dd10e0d655b805a82be4b454a864b
dbale-altoros <diego.bale@altoros.com>
2025-03-10 16:29:05 -0300

    fix: typo 3

--- scripts/generate-rule-docs.js
+++ scripts/generate-rule-docs.js
@@ -1,10 +1,10 @@
 #!env node
-const { loadRules } = require('../lib/load-rules')
 const fs = require('fs')
 const { exec, mkdir } = require('shelljs')
 const semver = require('semver')
 const path = require('path')
 const table = require('markdown-table')
+const { loadRules } = require('../lib/load-rules')
 const { ruleSeverityEnum } = require('../lib/doc/utils')
 
 /**
@@ -151,7 +151,7 @@ function loadOptions(rule) {
 ${table(optionsTable)}
 `
   } else if (typeof rule.meta.defaultSetup === 'string') {
-    return `This rule accepts a string option of rule severity. Must be one of ${ruleSeverityEnum}. Defaults to ${rule.meta.defaultSetup}.`
+    return `This rule accepts a string option for rule severity. Must be one of ${ruleSeverityEnum}. Defaults to ${rule.meta.defaultSetup}.`
   } else {
     throw new Error(`Unhandled type of rule.meta.defaultSetup from rule ${rule.ruleId}`)
   }
@@ -175,7 +175,7 @@ function loadNotes(rule) {
     textToReturn = `### Notes\n`
     for (let i = 0; i < rule.meta.docs.notes.length; i++) {
       noteValue = rule.meta.docs.notes[i].note
-      textToReturn = textToReturn + `- ${noteValue}\n`
+      textToReturn += `- ${noteValue}\n`
     }
   }
 
@@ -186,7 +186,7 @@ function linkToVersion(version) {
   if (version) {
     return `This rule was introduced in [Solhint ${version}](https://github.com/protofire/solhint/blob/v${version})`
   } else {
-    return `This rule is introduced in the latest version.`
+    return `This rule was introduced in the latest version.`
   }
 }
 
@@ -196,9 +196,7 @@ function linkToSource(rule) {
 }
 
 function linkToDocumentSource(rule) {
-  const link = localPathToUri(rule.file)
-    .replace('lib/rules', 'docs/rules')
-    .replace(/\.js$/, '.md')
+  const link = localPathToUri(rule.file).replace('lib/rules', 'docs/rules').replace(/\.js$/, '.md')
   return `https://github.com/protofire/solhint/blob/master${link}`
 }
 
@@ -208,7 +206,10 @@ function linkToTestCase(rule) {
 }
 
 function localPathToUri(file) {
-  return file.replace(path.resolve(path.join(__dirname, '..')), '').split(path.sep).join('/')
+  return file
+    .replace(path.resolve(path.join(__dirname, '..')), '')
+    .split(path.sep)
+    .join('/')
 }
 
 function loadExamples(rule) {
